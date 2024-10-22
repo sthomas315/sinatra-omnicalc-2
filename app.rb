0fdc262d2d549c187e18ce3ccf5c89b60c692b2a
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-10-22 12:29:30 +0000

    many

--- app.rb
+++ app.rb
@@ -5,38 +5,44 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
 end
 
 get("/subtract") do
-  erb(:sub_form)
+  erb(:subtraction_form)
 end
 
 get("/wizard_subtract") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
+  @first_sub = params.fetch("first_subtract").to_f
+  @second_sub = params.fetch("second_subtract").to_f
 
+  @result_sub = @first_sub - @second_sub
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_mul = params.fetch("multiply_first").to_f
+  @second_mul = params.fetch("multiply_again").to_f
+
+  @result_mul = @first_mul * @second_mul
   erb(:mult_result)
 end
 
 get("/divide") do
-  erb(:div_form)
+  erb(:divform)
 end
 
 get("/wizard_divide") do
